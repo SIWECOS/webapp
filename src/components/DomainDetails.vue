c255bc232cd7d64b73f1862687fbd565cf3ceaf5
David Jardin <d.jardin@djumla.de>
2018-02-23 14:22:57 +0100

    Display additional data in scan results

--- src/components/DomainDetails.vue
+++ src/components/DomainDetails.vue
@@ -19,45 +19,24 @@
             <canvas width="100" height="100"></canvas>
         </div>
 
-        <div class="last-scan-data" v-if="result"><span>{{ $t('messages.lastScan') }}<br> {{ result.scanFinished }}</span></div>
+        <div class="last-scan-data" v-show="result"><span>{{ $t('messages.lastScan') }}<br> {{ result.scanFinished }}</span></div>
 
-        <div class="scanners-wrapper" v-if="result && showDetails">
+        <div class="scanners-wrapper" v-show="result && showDetails">
             <div class="scanner-content" v-for="(scanner) in result.scanners">
-                <div class="scanner-item-data">
-                    <span class="scanner-data">{{ scanner.scanner_type }}</span>
-                    <small><a :href="'https://www.siwecos.de/wiki/' + scanner.scanner_type" title="Mehr Informationen" target="_blank">Mehr Informationen &gt;&gt;</a></small>
-                    <br><span><small>{{ $t('messages.lastScan') }}: {{ scanner.updated_at }}</small></span><p></p>
-
-                    <div class="scanner-check-wrapper">
-                        <div class="scanner-check-content" v-for="(scanresult) in scanner.result">
-                            <div class="scanner-check-item-data">
-                                <span class="round-check">{{ scanresult.score }}</span>
-                                <span class="scanner-check-data">{{ scanresult.name}}</span>
-
-                                <small><a :href="'https://www.siwecos.de/wiki/' + scanresult.name" title="Mehr Informationen" target="_blank">Mehr Informationen &gt;&gt;</a></small>
-
-                                <ul class="scanner-check-item-details" v-if="scanresult.testDetails">
-                                    <li v-for="(detail) in scanresult.testDetails">{{ detail }}</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="impact-gauge gaugeMeter" :data-percent="scanner.score" data-size="75" data-width="14" data-style="Full" data-theme="Red-Gold-Green" data-animate_gauge_colors="1" style="width: 75px;">
-                        <span style="line-height: 75px; font-size: 16.5px;">{{ scanner.score }}</span><canvas width="75" height="75"></canvas>
-                    </div>
-                </div>
+                <scanner-details v-bind:scanner="scanner"></scanner-details>
             </div>
         </div>
 
-        <span v-if="msg">{{ $t('messages.' + msg) }}</span>
+        <span v-show="msg">{{ $t('messages.' + msg) }}</span>
     </div>
 </template>
 
 <script>
 import api from '../services/api.js'
+import ScannerDetails from './ScannerDetails'
 
 export default {
+  components: {ScannerDetails},
   name: 'DomainDetails',
   data () {
     return {
@@ -83,6 +62,9 @@ export default {
           this.msg = false
         }.bind(this), 5000)
       })
+    },
+    toggleSlide (slider) {
+      slider.showDetails = (slider.showDetails) ? 0 : 1
     }
   },
   created: function () {
@@ -102,7 +84,8 @@ export default {
           start_error: 'Fehler beim Starten des Scans, bitte versuchen Sie es später erneut',
           start_success: 'Scan erfolgreich gestartet, die Ergebnisse stehen in einigen Minuten zur Verfügung',
           lastScan: 'Letzter Scan',
-          showResults: 'Zeige Ergebnisse'
+          showResults: 'Zeige Ergebnisse',
+          more_info: 'Mehr Informationen'
         }
       }
     }
