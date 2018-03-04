38cae83f85d7f67f4d40b0a206f4d7193675ec90
Marcel Wege <mwege@byte5.de>
2018-03-04 17:19:25 +0100

    Soooo beautiful :)

--- src/components/DomainDetails.vue
+++ src/components/DomainDetails.vue
@@ -14,6 +14,7 @@
             </li>
         </ul>
         <div style="padding-bottom: 25px;" class="clear: both"></div>
+      <h3>Gesamtergebnis</h3>
         <div class="impact-gauge gaugeMeter" :data-percent="result.weightedMedia.toFixed(0)" data-size="100" data-width="20" data-style="Arch" data-theme="Red-Gold-Green" data-animate_gauge_colors="1" style="width: 100px;" v-if="result">
 
         </div>
