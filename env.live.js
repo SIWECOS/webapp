160a951675e58fad1887e33b8c22d7e192e9bd9a
David Jardin <d.jardin@djumla.de>
2019-09-24 09:18:43 +0200

    adjust token name

--- env.live.js
+++ env.live.js
@@ -2,5 +2,5 @@ export default {
   APP_NAME: 'SIWECOS',
   APP_ENV: 'prod',
   APP_URL: 'https://api.siwecos.de',
-  ID_TOKEN: 'id_token'
+  ID_TOKEN: 'siwecos_token'
 }
--- env.staging.js
+++ env.staging.js
@@ -2,5 +2,5 @@ export default {
   APP_NAME: 'SIWECOS',
   APP_ENV: 'dev',
   APP_URL: 'https://api.staging.siwecos.de',
-  ID_TOKEN: 'id_token'
+  ID_TOKEN: 'siwecos_token'
 }
